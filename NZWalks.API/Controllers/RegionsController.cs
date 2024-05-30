b5f7e2fd99bb193572d6a5e10850324f82444f18
sifloyd <gsabarna@gmail.com>
2024-05-31 02:29:23 +0530

    changes as of 31/5/2024

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetRegions()
+        {
+
+           var regions = mapper.Map<List<RegionDto>>(regionRepository.GetRegions());
+
+           return Ok(regions);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRegionById(Guid id)
+        {
+            var region = mapper.Map <RegionDto> (regionRepository.GetRegionById(id));
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRegion([FromBody] RegionDto regionDto)
+        {
+
+            regionRepository.CreateRegion(mapper.Map<Region>(regionDto));
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateRegion(Guid id, [FromBody] RegionDto regionDto)
+        {
+            var region = regionRepository.GetRegionById(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            regionRepository.UpdateRegion(mapper.Map<Region>(regionDto));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRegion(Guid id)
+        {
+            var region = regionRepository.GetRegionById(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            regionRepository.DeleteRegion(region);
+
+            return NoContent();
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Mappings;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NzWalksConnectionString")));
+
+builder.Services.AddScoped<IRegionRepository, SqlRegionRepository>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 var app = builder.Build();
 
